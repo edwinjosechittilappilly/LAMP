4bd414df6e792aa57b2c825bb98a52b2fe32b8a1
zvendetta <lamp@localhost.localdomain>
2018-02-27 18:47:48 +0530

    gpio added

--- features.py
+++ features.py
@@ -1,72 +1,20 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM);
-f = 5
-b = 6
-l = 13
-r = 19
-lights = 26
-GPIO.setup(f, GPIO.OUT, initial = 0)
-GPIO.setup(b, GPIO.OUT, initial = 0)
-GPIO.setup(l, GPIO.OUT, initial = 0)
-GPIO.setup(r, GPIO.OUT, initial = 0)
-GPIO.setup(lights, GPIO.OUT,initial = 0);
-    
-def ret():
-        file= open("mot.txt",'r')
-        ff=file.readlines()
-        file.close();
-        va=ff[-1]
-        var=str(va)
-        print(var);
-        if "1000" in var:
-            GPIO.output(5, GPIO.HIGH)
-            GPIO.output(6, GPIO.HIGH)
-            GPIO.output(13, GPIO.LOW)
-            GPIO.output(19, GPIO.LOW)
-            
-            print("I am bending forward back")
-            #GPIO.cleanup()
-            time.sleep(5);
-            #return "I am bending forward back"
-        if "0100" in var:
-            GPIO.output(5, GPIO.HIGH)
-            GPIO.output(6, GPIO.LOW)
-            GPIO.output(13, GPIO.HIGH)
-            GPIO.output(19, GPIO.LOW)
-            print("I am bending backward back")
-            time.sleep(5);
-            #GPIO.cleanup()
-            #return "I am bending backward back"
-        if "0010" in var:
-            GPIO.output(13, GPIO.HIGH)
-            GPIO.output(6, GPIO.HIGH)
-            print("I am bending left back")
-            time.sleep(5);
-            #GPIO.cleanup()
-            #return "I am bending left back"
-        if "0001" in var:
-            GPIO.output(19, GPIO.HIGH)
-            GPIO.output(6, GPIO.HIGH)
-            print("I am bending right back")
-            time.sleep(5);
-            #GPIO.cleanup()
-            #return "I am bending right back"
 
 class motion:
     "has functions related to the motion of the lamp"
-    GPIO.setmode(GPIO.BCM);
-    f = 5
-    b = 6
-    l = 13
-    r = 19
-    lights = 26
-    GPIO.setup(f, GPIO.OUT, initial = 0)
-    GPIO.setup(b, GPIO.OUT, initial = 0)
-    GPIO.setup(l, GPIO.OUT, initial = 0)
-    GPIO.setup(r, GPIO.OUT, initial = 0)
-    GPIO.setup(lights, GPIO.OUT, initial = 0)
     def mov(s):
+        GPIO.setmode(GPIO.BCM);
+        f = 5
+        b = 6
+        l = 13
+        r = 19
+        lights = 26
+        GPIO.setup(f, GPIO.OUT, initial = 0)
+        GPIO.setup(b, GPIO.OUT, initial = 0)
+        GPIO.setup(l, GPIO.OUT, initial = 0)
+        GPIO.setup(r, GPIO.OUT, initial = 0)
+        GPIO.setup(lights, GPIO.OUT,initial = 0)
         #general movement
         if s=="F" or s=="f":
             #ret()
@@ -79,8 +27,8 @@ class motion:
             GPIO.output(13, GPIO.LOW)
             GPIO.output(19, GPIO.LOW)
             print("I am bending forward")
-            time.sleep(5)
-            #GPIO.cleanup()
+            time.sleep(3)
+            GPIO.cleanup()
             return "I am bending forward"
         if s=="B" or s=="b":
             #ret()
@@ -93,7 +41,8 @@ class motion:
             GPIO.output(13, GPIO.LOW)
             GPIO.output(19, GPIO.LOW)
             print("I am bending backward")
-            time.sleep(5)
+            time.sleep(3)
+            GPIO.cleanup()
             return "I am bending backward"
         if s=="l" or s=="L":
             #ret()
@@ -106,7 +55,8 @@ class motion:
             GPIO.output(6, GPIO.LOW)
             GPIO.output(19, GPIO.LOW)
             print("I am bending left")
-            time.sleep(5)
+            time.sleep(3)
+            GPIO.cleanup()
             return "I am bending left"
         if s=="R" or s=="r":
             #ret()
@@ -119,7 +69,8 @@ class motion:
             GPIO.output(6, GPIO.LOW)
             GPIO.output(13, GPIO.LOW)
             print("I am bending Right")
-            time.sleep(5)
+            time.sleep(3)
+            GPIO.cleanup()
             return "I am bending Right"
         if s=="t" or s=="T":
             #ret()
@@ -127,16 +78,30 @@ class motion:
             GPIO.output(6, GPIO.HIGH)
             GPIO.output(13, GPIO.HIGH)
             GPIO.output(19, GPIO.HIGH)
-            time.sleep(5)
+            time.sleep(3)
+            GPIO.cleanup()
             return "testing"
     def light(glow):
+        GPIO.setmode(GPIO.BCM);
+        f = 5
+        b = 6
+        l = 13
+        r = 19
+        lights = 26
+        GPIO.setup(f, GPIO.OUT, initial = 0)
+        GPIO.setup(b, GPIO.OUT, initial = 0)
+        GPIO.setup(l, GPIO.OUT, initial = 0)
+        GPIO.setup(r, GPIO.OUT, initial = 0)
+        GPIO.setup(lights, GPIO.OUT,initial = 0)
         if glow=="on":
             GPIO.output(126, GPIO.HIGH)
             print("Lights turned on")
+            GPIO.cleanup()
             return "Lights turned on"
         if glow=="off":
             GPIO.output(126, GPIO.HIGH)
             print("Lights turned on")
+            GPIO.cleanup()
             return "Lights turned on"
         
         
