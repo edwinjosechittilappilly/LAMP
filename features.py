5bb711e51fc601e847bbc1bc36f4b8156097e142
Edwin Jose <edwinjose900@gmail.com>
2018-02-27 06:35:46 +0500

    new files added

--- features.py
+++ features.py
@@ -17,19 +17,20 @@ class motion:
     GPIO.output(r, GPIO.LOW)
     def mov(s):
         if s=="F" or s=="f":
-            GPIO.output(f, GPIO.HIGH)
+            GPIO.output(5, GPIO.HIGH)
             print("I am bending forward")
+            #GPIO.cleanup()
             return "I am bending forward"
         if s=="B" or s=="b":
-            GPIO.output(b, GPIO.HIGH)
+            GPIO.output(6, GPIO.HIGH)
             print("I am bending backward")
             return "I am bending backward"
         if s=="l" or s=="L":
-            GPIO.output(l, GPIO.HIGH)
+            GPIO.output(13, GPIO.HIGH)
             print("I am bending left")
             return "I am bending left"
         if s=="R" or s=="r":
-            GPIO.output(r, GPIO.HIGH)
+            GPIO.output(19, GPIO.HIGH)
             print("I am bending Right")
             return "I am bending Right"
         
